2ece795c36f0e4c65fe0937bc5edb705c08bde9d
Ävadakedavr0 <dylandd4@hotmail.be>
2024-04-07 20:44:31 +0200

    Adding css and NavMenu

--- GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
+++ GameStore.Frontend/obj/Debug/net8.0/GameStore.Frontend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+795b0b46e2ce5e79c7ce81cc99849dd81f466797")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dcda7cd5d8255fb4b4db198e06990d08777823c")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Frontend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
